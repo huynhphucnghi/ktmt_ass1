39cf011ffe4727a61a712ad2ad82358d499bf450
Vũ Đức Duy <duyisking1060@gmail.com>
2018-05-09 08:49:33 +0700

    Connect system to instruction data

--- system.v
+++ system.v
@@ -4,9 +4,22 @@ module system(
 			input		SYS_load,
 			input [7:0]	SYS_pc_val,
 			input [7:0]	SYS_output_sel,
-			input [31:0] SYS_dram_input,
-			output [26:0]SYS_leds,
-			output [12:0]SYS_dram_addr
+			output [26:0]SYS_leds
 );
+
+reg [31:0] addr = 32'b0;
+wire [4:0] temp;
+
+always @(posedge SYS_clk) begin
+	addr = (addr + 1'b1);
+end
+
+test _test(
+	addr,
+	SYS_load,
+	32'h0a0a0a0a,
+	!SYS_reset,
+	{temp, SYS_leds}
+	);
 
 endmodule
--- system_interface.v
+++ system_interface.v
@@ -1,21 +1,17 @@
 module system_interface(
-			input	[3:0]	KEY,
-			input [31:0] DRAM_DQ,
+			input [3:0] KEY,
 			input [17:0] SW,
 			output [17:0]LEDR,
-			output [8:0] LEDG,
-			output [12:0] DRAM_ADDR
+			output [8:0] LEDG
 );
 
-system(
+system _system(
 			KEY[0],
-			KEY[1],
 			KEY[2],
-			SW[7:0],
+			KEY[1],
+			SW[10:3],
 			SW[17:10],
-			DRAM_DQ,
-			{LEDG, LEDR},
-			DRAM_ADDR
+			{LEDR, LEDG}
 );
 
 endmodule
